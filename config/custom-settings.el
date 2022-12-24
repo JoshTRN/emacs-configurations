458b35221fef3396e39852827608f9fe480324c8
Wood, Joshua T <Joshua.T.Wood@uscis.dhs.gov>
2022-12-24 02:18:10 -0500

    Refactor settings

--- config/custom-settings.el
+++ config/custom-settings.el
@@ -19,34 +19,16 @@
       vterm-max-scrollback 100000
       vterm-timer-delay 0.01)
 
-(setq-default visual-fill-column-center-text t
+(setq-default fill-column 110
+              highlight-indent-guides-character 9615
+              visual-fill-column-center-text t
               visual-fill-column-width 110
-              valign-fancy-bar 't
-              fill-column 110)
-
-(defun pretty-print ()
-  (visual-fill-column-mode 1)
-  (visual-line-mode 1))
-
-(defun pretty-print-header ()
-  (pretty-print)
-  (setq-local face-remapping-alist '((header-line (:height 4.0) variable-pitch)))
-  (setq header-line-format " "))
-
-(defun my-yaml-hook ()
-  (spacemacs/toggle-absolute-line-numbers-on)
-  (highlight-indentation-mode 1))
+              valign-fancy-bar 't)
 
 (defun org-babel-tangle-block()
   (interactive)
   (let ((current-prefix-arg '(4)))
     (call-interactively 'org-babel-tangle)))
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
 (defun my-correct-symbol-bounds (pretty-alist)
   "Prepend a TAB character to each symbol in this alist,
@@ -69,6 +51,17 @@
   (cl-letf (((symbol-function 'y-or-n-p) (lambda (prompt) t)))
     (apply orig-fun args)))
 
+(defun evil-jump-backward-centered (&optional count)
+  (interactive)
+  (evil-jump-backward count)
+  (evil-scroll-line-to-center count))
+
+
+(defun evil-jump-forward-centered (&optional count)
+  (interactive)
+  (evil-jump-forward count)
+  (evil-scroll-line-to-center count))
+
 
 (defun my-setup-indent (n)
   (setq javascript-indent-level n)
@@ -79,6 +72,19 @@
   (setq web-mode-code-indent-offset n)
   (setq css-indent-offset n)
   (setq groovy-indent-offset n))
+
+(defun pretty-print ()
+  (visual-fill-column-mode 1)
+  (visual-line-mode 1))
+
+(defun pretty-print-header ()
+  (pretty-print)
+  (setq-local face-remapping-alist '((header-line (:height 4.0) variable-pitch)))
+  (setq header-line-format " "))
+
+(defun my-yaml-hook ()
+  (spacemacs/toggle-absolute-line-numbers-on)
+  (highlight-indentation-mode 1))
 
 (defun my-org-mode-hook ()
   (setq org-hide-leading-stars nil)
@@ -94,13 +100,22 @@
   (pretty-print-header)
   (solaire-mode -1))
 
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+
 (require 'dap-node)
 (require 'dap-chrome)
 (require 'dap-firefox)
 
+(remove-hook 'org-present-mode-hook 'spacemacs//org-present-start)
+
 (add-hook 'markdown-mode-hook 'pretty-print-header)
-(add-hook 'help-mode-hook 'visual-line-mode)
-(add-hook 'help-mode-hook 'visual-fill-column-mode)
+(add-hook 'help-mode-hook 'pretty-print)
+(add-hook 'lsp-help-mode-hook 'pretty-print)
 (add-hook 'yaml-mode-hook 'my-yaml-hook)
 (add-hook 'haskell-mode-hook 'pretty-lambdas-haskell)
 (add-hook 'elm-mode-hook 'pretty-lambdas-haskell)
@@ -120,6 +135,10 @@
 (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
 (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
 (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+
+(define-key evil-motion-state-map (kbd "<remap> <evil-jump-backward>") 'evil-jump-backward-centered)
+(define-key evil-motion-state-map (kbd "<remap> <evil-jump-forward>") 'evil-jump-forward-centered)
+
 
 (spacemacs/set-leader-keys-for-major-mode 'elm-mode "fr" 'lsp-ui-peek-find-references)
 (spacemacs/set-leader-keys-for-major-mode 'elm-mode "ug" 'lsp-ui-doc-glance)
@@ -148,16 +167,41 @@
    (shell  . t)
    (js     . t)))
 
-(eval-after-load "org"
-  '(progn
-     (define-key org-mode-map (kbd "C-c b") 'org-babel-tangle-block)))
-
 (my-setup-indent 2)
 
 (spacemacs/toggle-vi-tilde-fringe-off)
 
 (solaire-global-mode +1)
 
+(pixel-scroll-precision-mode)
+
 (make-variable-buffer-local 'global-hl-line-mode)
 
 (advice-add 'ediff-quit :around #'disable-y-or-n-p)
+
+(eval-after-load "org"
+  '(progn
+     (define-key org-mode-map (kbd "C-c b") 'org-babel-tangle-block)))
+
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-display-inline-images)
+                 (evil-define-key 'normal org-present-mode-keymap
+                   (kbd "<left>")  'org-present-prev
+                   (kbd "<right>") 'org-present-next
+                   "q"             'org-present-quit)
+                 (text-scale-set 2)
+                 (setq visual-fill-column-width 50)
+                 (funcall(lambda ()
+                           (message "setting org shift tab")
+                           (org-shifttab)
+                           ))
+                 ))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-remove-inline-images)
+                 (text-scale-set 1)
+                 (setq visual-fill-column-width 100)
+                 ))))
